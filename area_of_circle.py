056d1a03f4c98bbab0c7d3d306522f6aa805dfbe
balamurugan <saathvik1063bala@gmail.com>
2024-12-01 19:17:04 +0530

    adding the file

---
+++ area_of_circle.py
@@ -0,0 +1,19 @@
+#find the area of circle and its parimeter
+# area of circle formula area = pi x radius **2
+# circle of permeter = 2 x pi x radius
+
+# write user input for radius
+
+#use math in python use math 
+import math
+
+radius=  float(input("enter the ratio:"))
+
+
+area_of_circle = 3.14 * radius ** 2
+
+circle_of_perimeter = 2*3.14*radius
+
+
+print(f"area of the circle is: {area_of_circle:.2f}")
+print(f"circle of perimeter is : {circle_of_perimeter:.2f}")
